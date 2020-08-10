54f2db2f56093afbdca93661f26623301f4d16d8
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-10 07:54:38 +0700

    memindahkan table code ke dalam div class row

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -5,26 +5,32 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 	</head>
 	<body>
-	<h1>Data Siswa</h1>
-	<table>
-		<tr>
-			<th>NAMA DEPAN</th>
-			<th>NAMA BELAKANG</th>
-			<th>JENIS KELAMIN</th>
-			<th>AGAMA</th>
-			<th>ALAMAT</th>
-		</tr>
-		@foreach($data_siswa as $siswa)
-		<tr>
-			<td>{{$siswa->nama_depan}}</td>
-			<td>{{$siswa->nama_belakang}}</td>
-			<td>{{$siswa->jenis_kelamin}}</td>
-			<td>{{$siswa->agama}}</td>
-			<td>{{$siswa->alamat}}</td>
-		</tr>
-		@endforeach
-	</table>
-		
+
+			<div class="container">
+				<div class="row">
+					<h1>Data Siswa</h1>
+					<table>
+						<tr>
+							<th>NAMA DEPAN</th>
+							<th>NAMA BELAKANG</th>
+							<th>JENIS KELAMIN</th>
+							<th>AGAMA</th>
+							<th>ALAMAT</th>
+						</tr>
+						@foreach($data_siswa as $siswa)
+						<tr>
+							<td>{{$siswa->nama_depan}}</td>
+							<td>{{$siswa->nama_belakang}}</td>
+							<td>{{$siswa->jenis_kelamin}}</td>
+							<td>{{$siswa->agama}}</td>
+							<td>{{$siswa->alamat}}</td>
+						</tr>
+						@endforeach
+					</table>
+				</div>
+			</div>
+	
+
 
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
