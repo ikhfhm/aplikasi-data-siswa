c2aa8c2003f0e7237939b708c525c017c3424b5f
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-11 06:11:49 +0700

    mengganti Email address dengan Nama Depan pada index.blade.php

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -51,7 +51,7 @@
       <div class="modal-body">
         <form>
 		  <div class="form-group">
-		    <label for="exampleInputEmail1">Email address</label>
+		    <label for="exampleInputEmail1">Nama Depan</label>
 		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
 		    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 		  </div>
