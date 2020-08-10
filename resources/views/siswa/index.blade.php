a811c85de88e3c779baba74bb20afeb2eb522501
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-10 12:21:08 +0700

    mengganti ukuran button Tambah data siswa dengan btn-sm

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -12,7 +12,7 @@
 						<h1>Data Siswa</h1>
 					</div>
 					<div class="col-6">
-						<button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
+						<button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#exampleModal">
 						  Tambah data siswa
 						</button>
 					</div>
@@ -57,7 +57,7 @@
       </div>
     </div>
   </div>
-</div>																																																																																																																		
+</div>															
 
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
