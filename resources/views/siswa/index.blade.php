3d147d61582bd24be72218b4c077f7913bef7871
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-11 15:19:38 +0700

    menambahkan action dan method post pada form pada index.blade.php

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -49,7 +49,7 @@
         </button>
       </div>
       <div class="modal-body">
-        <form>
+        <form action="/siswa/create" method="post">
 		  <div class="form-group">
 		    <label for="exampleInputEmail1">Nama Depan</label>
 		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Depan">
