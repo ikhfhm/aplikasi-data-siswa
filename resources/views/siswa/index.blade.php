af4d474d911668aac96f26b1b05158e9b1c01317
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-11 14:50:05 +0700

    menambahkan form controls dari Bootstrap pada index.blade.php

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -59,7 +59,16 @@
 		    <label for="exampleInputEmail1">Nama Belakang</label>
 		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Belakang">
 		  </div>
-		 
+		 	 <div class="form-group">
+			    <label for="exampleFormControlSelect1">Example select</label>
+			    <select class="form-control" id="exampleFormControlSelect1">
+			      <option>1</option>
+			      <option>2</option>
+			      <option>3</option>
+			      <option>4</option>
+			      <option>5</option>
+			    </select>
+			  </div>
 		  <button type="submit" class="btn btn-primary">Submit</button>
 		</form>
       </div>
