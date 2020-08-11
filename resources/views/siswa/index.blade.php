1f5b56e0a6dafe0c35d3749b5a4c23ab97d0985e
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-11 14:43:09 +0700

    menghapus email help pada index.blade.php

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -53,7 +53,6 @@
 		  <div class="form-group">
 		    <label for="exampleInputEmail1">Nama Depan</label>
 		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-		    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 		  </div>
 		 
 		  <button type="submit" class="btn btn-primary">Submit</button>
