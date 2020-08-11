cf7b3a22257521c4ff05d67e720c04a00573eec1
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-11 15:01:22 +0700

    mengedit form controls

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -59,14 +59,12 @@
 		    <label for="exampleInputEmail1">Nama Belakang</label>
 		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Belakang">
 		  </div>
+
 		 	 <div class="form-group">
-			    <label for="exampleFormControlSelect1">Example select</label>
+			    <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
 			    <select class="form-control" id="exampleFormControlSelect1">
-			      <option>1</option>
-			      <option>2</option>
-			      <option>3</option>
-			      <option>4</option>
-			      <option>5</option>
+			      <option>Laki-laki</option>
+			      <option>Perempuan</option>
 			    </select>
 			  </div>
 		  <button type="submit" class="btn btn-primary">Submit</button>
