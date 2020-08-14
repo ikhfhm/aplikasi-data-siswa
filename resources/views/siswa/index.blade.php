a731fa6fe00353db25951e5598ed99cd5eca8ba9
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-14 23:14:50 +0700

    menambahkan modal

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -48,6 +48,56 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="/siswa/create" method="post">
+        	{{csrf_field()}}
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Nama Depan</label>
+		    <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Depan">
+		  </div>
+
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Nama Belakang</label>
+		    <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Nama Belakang">
+		  </div>
+
+		  <div class="form-group">
+		  	<label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
+			<select name="jenis_kelamin" class="form-control" id="exampleFormControlSelect1">
+			   <option value="L">Laki-laki</option>
+			   <option value="P">Perempuan</option>
+			</select>
+		  </div>
+
+		  <div class="form-group">
+			<label for="exampleInputEmail1">Agama</label>
+		    <input name="agama" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Agama">
+		  </div>
+
+		  <div class="form-group">
+		    <label for="exampleFormControlTextarea1">Alamat</label>
+		    <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+		  </div>
+
+		 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+  </div>
 @stop
 @section('content1')
 				@if(session('sukses'))
