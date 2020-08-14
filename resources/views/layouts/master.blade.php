09a5c061b224803fe222fc1575c0e0fa2fd27873
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-14 17:35:48 +0700

    menghapus beberapa sidebar dengan menyisakan Dashboard dan Siswa

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -27,7 +27,7 @@
     <!-- NAVBAR -->
     <nav class="navbar navbar-default navbar-fixed-top">
       <div class="brand">
-        <a href="index.html"><img src="{{asset('admin/assets/img/logo-dark.png')}}" alt="Klorofil Logo" class="img-responsive logo"></a>
+        <a href="index.html"><img src="{{asset('admin/logo-kingsarmy.png')}}" alt="Klorofil Logo" class="img-responsive logo"></a>
       </div>
       <div class="container-fluid">
         <div class="navbar-btn">
@@ -87,23 +87,8 @@
         <nav>
           <ul class="nav">
             <li><a href="index.html" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
-            <li><a href="elements.html" class=""><i class="lnr lnr-code"></i> <span>Elements</span></a></li>
-            <li><a href="charts.html" class=""><i class="lnr lnr-chart-bars"></i> <span>Charts</span></a></li>
-            <li><a href="panels.html" class=""><i class="lnr lnr-cog"></i> <span>Panels</span></a></li>
-            <li><a href="notifications.html" class=""><i class="lnr lnr-alarm"></i> <span>Notifications</span></a></li>
-            <li>
-              <a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Pages</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
-              <div id="subPages" class="collapse ">
-                <ul class="nav">
-                  <li><a href="page-profile.html" class="">Profile</a></li>
-                  <li><a href="page-login.html" class="">Login</a></li>
-                  <li><a href="page-lockscreen.html" class="">Lockscreen</a></li>
-                </ul>
-              </div>
-            </li>
-            <li><a href="tables.html" class=""><i class="lnr lnr-dice"></i> <span>Tables</span></a></li>
-            <li><a href="typography.html" class=""><i class="lnr lnr-text-format"></i> <span>Typography</span></a></li>
-            <li><a href="icons.html" class=""><i class="lnr lnr-linearicons"></i> <span>Icons</span></a></li>
+            <li><a href="/siswa" class=""><i class="lnr lnr-code"></i> <span>Siswa</span></a></li>
+            
           </ul>
         </nav>
       </div>
