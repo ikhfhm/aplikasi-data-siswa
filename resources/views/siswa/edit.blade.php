193d8991800ce483d7c23354d5e921745105f7d0
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-15 08:48:40 +0700

    menambahkan kerangka form dari klorofil

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -1,6 +1,61 @@
 @extends('layouts.master')
 
 @section('content')
+	<div class="main">
+		<div class="main-content">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="panel">
+								<div class="panel-heading">
+									<h3 class="panel-title">Inputs</h3>
+								</div>
+								<div class="panel-body">
+									<input type="text" class="form-control" placeholder="text field">
+									<br>
+									<input type="password" class="form-control" value="asecret">
+									<br>
+									<textarea class="form-control" placeholder="textarea" rows="4"></textarea>
+									<br>
+									<select class="form-control">
+										<option value="cheese">Cheese</option>
+										<option value="tomatoes">Tomatoes</option>
+										<option value="mozarella">Mozzarella</option>
+										<option value="mushrooms">Mushrooms</option>
+										<option value="pepperoni">Pepperoni</option>
+										<option value="onions">Onions</option>
+									</select>
+									<br>
+									<label class="fancy-checkbox">
+										<input type="checkbox">
+										<span>Fancy Checkbox 1</span>
+									</label>
+									<label class="fancy-checkbox">
+										<input type="checkbox">
+										<span>Fancy Checkbox 2</span>
+									</label>
+									<label class="fancy-checkbox">
+										<input type="checkbox">
+										<span>Fancy Checkbox 3</span>
+									</label>
+									<br>
+									<label class="fancy-radio">
+										<input name="gender" value="male" type="radio">
+										<span><i></i>Male</span>
+									</label>
+									<label class="fancy-radio">
+										<input name="gender" value="female" type="radio">
+										<span><i></i>Female</span>
+									</label>
+								</div>
+							</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@stop
+@section('content1')
 				<h1>Edit data siswa</h1>
 				@if(session('sukses'))
 				<div class="alert alert-success" role="alert">
