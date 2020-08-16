15595c5b3686aeeea56f8640f77406c3759ae017
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-16 23:37:23 +0700

    menambahkan kolom avatar pada model Siswa.php

--- app/Siswa.php
+++ app/Siswa.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Siswa extends Model
 {
     protected $table = 'siswa';
-    protected $fillable = ['nama_belakang', 'nama_depan', 'jenis_kelamin', 'agama', 'alamat'];
+    protected $fillable = ['nama_belakang', 'nama_depan', 'jenis_kelamin', 'agama', 'alamat', 'avatar'];
 }
