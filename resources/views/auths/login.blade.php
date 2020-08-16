1d8f2cf07fb35d7e07ab87b660416d7f32ae3399
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-16 19:00:36 +0700

    menyesuaikan penempatan logo

--- resources/views/auths/login.blade.php
+++ resources/views/auths/login.blade.php
@@ -30,7 +30,7 @@
 					<div class="left">
 						<div class="content">
 							<div class="header">
-								<div class="logo text-center"><img src="{{asset('admin/assets/logo-dark.png')}}" alt="Klorofil Logo"></div>
+								<div class="logo text-center"><img src="{{asset('admin/logo-kingsarmy.png')}}" alt="Klorofil Logo"></div>
 								<p class="lead">Login to your account</p>
 							</div>
 							<form class="form-auth-small" action="index.php">
