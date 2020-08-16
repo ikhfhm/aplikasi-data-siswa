90209c7da61adb5ec6832d5a1e313134eed1dd44
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-16 18:42:19 +0700

    membuat route login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/login', 'AuthController@login');
 
 Route::get('/dashboard', 'DashboardController@index');
 Route::get('/siswa', 'SiswaController@index');
