f69a2ea8d6cd6d8612277178ab65f8c46a270990
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-20 23:09:42 +0700

    mengganti class name siswa nama_depan dengan guru nama pada profile.blade.php (guru)

--- resources/views/guru/profile.blade.php
+++ resources/views/guru/profile.blade.php
@@ -28,7 +28,7 @@
 									<div class="overlay"></div>
 									<div class="profile-main">
 										<img src="" class="img-circle" alt="Avatar">
-										<h3 class="name">{{$siswa->nama_depan}}</h3>
+										<h3 class="name">{{$guru->nama}}</h3>
 										<span class="online-status status-available">Available</span>
 									</div>
 									<div class="profile-stat">
