4e7096d443a29adbc3ef8d98b7a877d375b1507d
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-20 23:07:52 +0700

    menambahkan use app guru dan guru find dolar id pada GuruController.php

--- app/Http/Controllers/GuruController.php
+++ app/Http/Controllers/GuruController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+Use App\Guru;
 
 class GuruController extends Controller
 {
     public function profile($id)
     {
-    	return view('guru.profile');
+    	$guru = Guru::find($id);
+    	return view('guru.profile',['guru' => $guru]);
     }
 }
