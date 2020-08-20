77ee8a86716b882b8bc3e545077e17420aa9e9a6
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-20 08:05:00 +0700

    menambahkan basic column chart dari library Highcharts pada profile.blade.php

--- resources/views/siswa/profile.blade.php
+++ resources/views/siswa/profile.blade.php
@@ -64,33 +64,36 @@
 								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
 								  Tambah Nilai
 								</button>
-							<div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Mata Pelajaran</h3>
+								<div class="panel">
+									<div class="panel-heading">
+										<h3 class="panel-title">Mata Pelajaran</h3>
+									</div>
+									<div class="panel-body">
+										<table class="table table-striped">
+											<thead>
+												<tr>
+													<th>KODE</th>
+													<th>NAMA</th>
+													<th>SEMESTER</th>
+													<th>NILAI</th>
+												</tr>
+											</thead>
+											<tbody>
+												@foreach($siswa->mapel as $mapel)
+												<tr>
+													<td>{{$mapel->kode}}</td>
+													<td>{{$mapel->nama}}</td>
+													<td>{{$mapel->semester}}</td>
+													<td>{{$mapel->pivot->nilai}}</td>
+												</tr>
+												@endforeach
+											</tbody>
+										</table>
+									</div>
 								</div>
-								<div class="panel-body">
-									<table class="table table-striped">
-										<thead>
-											<tr>
-												<th>KODE</th>
-												<th>NAMA</th>
-												<th>SEMESTER</th>
-												<th>NILAI</th>
-											</tr>
-										</thead>
-										<tbody>
-											@foreach($siswa->mapel as $mapel)
-											<tr>
-												<td>{{$mapel->kode}}</td>
-												<td>{{$mapel->nama}}</td>
-												<td>{{$mapel->semester}}</td>
-												<td>{{$mapel->pivot->nilai}}</td>
-											</tr>
-											@endforeach
-										</tbody>
-									</table>
+								<div class="panel">
+									<div id="chartNilai"></div>
 								</div>
-							</div>
 							</div>
 							<!-- END RIGHT COLUMN -->
 						</div>
@@ -140,4 +143,71 @@
 
 @section('footer')
 <script src="https://code.highcharts.com/highcharts.js"></script>
+<script>
+		Highcharts.chart('chartNilai', {
+	    chart: {
+	        type: 'column'
+	    },
+	    title: {
+	        text: 'Monthly Average Rainfall'
+	    },
+	    subtitle: {
+	        text: 'Source: WorldClimate.com'
+	    },
+	    xAxis: {
+	        categories: [
+	            'Jan',
+	            'Feb',
+	            'Mar',
+	            'Apr',
+	            'May',
+	            'Jun',
+	            'Jul',
+	            'Aug',
+	            'Sep',
+	            'Oct',
+	            'Nov',
+	            'Dec'
+	        ],
+	        crosshair: true
+	    },
+	    yAxis: {
+	        min: 0,
+	        title: {
+	            text: 'Rainfall (mm)'
+	        }
+	    },
+	    tooltip: {
+	        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+	        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+	            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+	        footerFormat: '</table>',
+	        shared: true,
+	        useHTML: true
+	    },
+	    plotOptions: {
+	        column: {
+	            pointPadding: 0.2,
+	            borderWidth: 0
+	        }
+	    },
+	    series: [{
+	        name: 'Tokyo',
+	        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+
+	    }, {
+	        name: 'New York',
+	        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+
+	    }, {
+	        name: 'London',
+	        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
+
+	    }, {
+	        name: 'Berlin',
+	        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+
+	    }]
+	});
+</script>
 @stop
