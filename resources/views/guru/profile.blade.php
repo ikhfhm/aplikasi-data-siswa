eef53d19094fc8a12d615cf2b230a59cff4de99f
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-20 23:28:53 +0700

    membuat kolum tanpa isi pada profile.blade.php (guru)

--- resources/views/guru/profile.blade.php
+++ resources/views/guru/profile.blade.php
@@ -59,7 +59,12 @@
 											</thead>
 											<tbody>
 												<tr>
-													
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
+													<td></td>
 												</tr>
 											</tbody>
 										</table>
