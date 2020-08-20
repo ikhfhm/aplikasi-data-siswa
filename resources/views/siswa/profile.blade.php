09639c8dcce533471696a0e9330b8b5483c5dd3d
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-20 17:38:44 +0700

    menambahkan a href pada nama guru di profile.blade.php

--- resources/views/siswa/profile.blade.php
+++ resources/views/siswa/profile.blade.php
@@ -90,7 +90,7 @@
 													<td>{{$mapel->nama}}</td>
 													<td>{{$mapel->semester}}</td>
 													<td><a href="#" class="nilai" data-type="text" data-pk="{{$mapel->id}}" data-url="/api/siswa/{{$siswa->id}}/editnilai" data-title="Masukkan Nilai">{{$mapel->pivot->nilai}}</a></td>
-													<td>{{$mapel->guru->nama}}</td>
+													<td><a href="#">{{$mapel->guru->nama}}</a></td>
 													<td><a href="/siswa/{{$siswa->id}}/{{$mapel->id}}/deletenilai" class="btn btn-danger btn-sm" onclick="return confirm('Yakin mau dihapus?')">Delete</a></td>
 												</tr>
 												@endforeach
