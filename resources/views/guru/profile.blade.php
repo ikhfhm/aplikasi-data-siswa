e09369bdc7b5a85dcdda11f0e9afa10e8cc39f89
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-20 23:27:06 +0700

    menghapus beberapa element pada profile.blade.php (guru)

--- resources/views/guru/profile.blade.php
+++ resources/views/guru/profile.blade.php
@@ -59,12 +59,7 @@
 											</thead>
 											<tbody>
 												<tr>
-													<td>{{$mapel->kode}}</td>
-													<td>{{$mapel->nama}}</td>
-													<td>{{$mapel->semester}}</td>
-													<td><a href="#" class="nilai" data-type="text" data-pk="{{$mapel->id}}" data-url="/api/siswa/{{$siswa->id}}/editnilai" data-title="Masukkan Nilai">{{$mapel->pivot->nilai}}</a></td>
-													<td><a href="/guru/{{$mapel->guru_id}}/profile">{{$mapel->guru->nama}}</a></td>
-													<td><a href="/siswa/{{$siswa->id}}/{{$mapel->id}}/deletenilai" class="btn btn-danger btn-sm" onclick="return confirm('Yakin mau dihapus?')">Delete</a></td>
+													
 												</tr>
 											</tbody>
 										</table>
