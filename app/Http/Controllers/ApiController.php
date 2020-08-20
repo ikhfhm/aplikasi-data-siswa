151346505c58bec9094868ad6e0e3f92f9f4fc70
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-20 15:01:34 +0700

    memodifikasi element pada function editnilai pada ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -8,6 +8,7 @@ class ApiController extends Controller
 {
     public function editnilai(Request $request, $id)
     {
-    	return $request->all();
+    	$siswa = \App\Siswa::find($id);
+    	$siswa->mapel()->updateExistingPivot($request->pk,['nilai' => $request->value]);
     }
 }
