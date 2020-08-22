6d8a19cad0b9f9e26a65d9fbc1256ec6d23df1a7
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-22 07:46:28 +0700

    menambahkan 2 use dari maatweb pada SiswaController.php

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use App\Exports\UsersExport;
+use Maatwebsite\Excel\Facades\Excel;
+
 
 class SiswaController extends Controller
 {
