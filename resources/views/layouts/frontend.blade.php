7bb50b078945ea7fc44a68a42f63fa4e04301103
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-22 22:04:48 +0700

    menambahkan {{asset('frontend')}}/ pada link css di frontend.blade.php

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -19,16 +19,16 @@
 		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
 			<!--
 			CSS
-			============================================= -->
-			<link rel="stylesheet" href="css/linearicons.css">
-			<link rel="stylesheet" href="css/font-awesome.min.css">
-			<link rel="stylesheet" href="css/bootstrap.css">
-			<link rel="stylesheet" href="css/magnific-popup.css">
-			<link rel="stylesheet" href="css/nice-select.css">							
-			<link rel="stylesheet" href="css/animate.min.css">
-			<link rel="stylesheet" href="css/owl.carousel.css">			
-			<link rel="stylesheet" href="css/jquery-ui.css">			
-			<link rel="stylesheet" href="css/main.css">
+			============================={{asset('/frontend')}}/================ -->
+			<link rel="stylesheet" href="{{asset('/frontend')}}/css/linearicons.css">
+			<link rel="stylesheet" href="{{asset('/frontend')}}/css/font-awesome.min.css">
+			<link rel="stylesheet" href="{{asset('/frontend')}}/css/bootstrap.css">
+			<link rel="stylesheet" href="{{asset('/frontend')}}/css/magnific-popup.css">
+			<link rel="stylesheet" href="{{asset('/frontend')}}/css/nice-select.css">							
+			<link rel="stylesheet" href="{{asset('/frontend')}}/css/animate.min.css">
+			<link rel="stylesheet" href="{{asset('/frontend')}}/css/owl.carousel.css">			
+			<link rel="stylesheet" href="{{asset('/frontend')}}/css/jquery-ui.css">			
+			<link rel="stylesheet" href="{{asset('/frontend')}}/css/main.css">
 		</head>
 		<body>	
 		  <header id="header" id="home">
