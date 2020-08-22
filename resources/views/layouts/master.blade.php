d611eaa86a90fd205f22faf6b19c527674775a57
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-22 18:26:36 +0700

    menambahkan script css dari toastr pada mster.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,6 +15,8 @@
   <!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
   <link rel="stylesheet" href="{{asset('admin/assets/css/demo.css')}}">
   <!-- GOOGLE FONTS -->
+  <!--TOASTR -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
   <!-- ICONS -->
   <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
