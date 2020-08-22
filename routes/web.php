5974c19ed88f2686f5b3e0b0194526c1b6a4d074
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-22 16:00:36 +0700

    mengganti {id} dengan siswa pada route update

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::group(['middleware' => ['auth','checkRole:admin,']],function(){
 	
 	Route::get('/siswa', 'SiswaController@index');
 	Route::get('/siswa/{siswa}/edit', 'SiswaController@edit');
-	Route::post('/siswa/{id}/update', 'SiswaController@update');
+	Route::post('/siswa/siswa/update', 'SiswaController@update');
 	Route::get('/siswa/{id}/delete', 'SiswaController@delete');
 	Route::get('/siswa/{siswa}/profile', 'SiswaController@profile');
 	Route::post('/siswa/{id}/addnilai', 'SiswaController@addnilai');
