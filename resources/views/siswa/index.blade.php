2841834377446433207489491dd84bce23563699
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-22 16:56:12 +0700

    menambahkan siswa-id pada a href delete pada index.blade.php

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -41,7 +41,7 @@
 												<td>{{$siswa->rataRataNilai()}}</td>
 												<td>
 													<a href="/siswa/{{$siswa->id}}/edit" class="btn btn-warning btn-sm">Edit</a>
-													<a href="#" class="btn btn-danger btn-sm">Delete</a>
+													<a href="#" class="btn btn-danger btn-sm" siswa-id="{{$siswa->id}}">Delete</a>
 												</td>
 											</tr>
 											@endforeach
