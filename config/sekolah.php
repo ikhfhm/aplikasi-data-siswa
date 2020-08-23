c0463a19432076493fe498721a4e423042ac80cd
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-23 23:39:48 +0700

    menambahkan beberapa config di sekolah.php dan menjadi objek dari home.blade.php dan frontend.php

--- config/sekolah.php
+++ config/sekolah.php
@@ -1,5 +1,31 @@
 <?php
 
 return [
-	'title' => 'SMA 1 Kingsarmy'
+	'title' => 'SMA 1 Kingsarmy',
+	'welcome_message' => 'Selamat Datang di Website SMA 1 Kingsarmy',
+	'sub_welcome_message' => 'Sekolah dengan kurikulum terbaik di dunia militer',
+	'welcome_message_button_text' => 'Jelajahi',
+	'welcome_message_button_url' => '/jelajahi',
+
+	'telpon' => '021 1234567',
+	'email' => 'sma1@kingsarmy.com',
+
+	'facebook_url' => 'https://facebook.com/',
+	'twitter_url' => 'https://twitter.com/',
+
+	'home_feature_column_1_title' => 'Fasilitas Memadai',
+	'home_feature_column_1_content' => 'Memiliki fasilitas yang memadai untuk mendukung kegiatan belajar mengajar',
+	'home_feature_column_1_link_text' => 'Lihat Fasilitas',
+	'home_feature_column_1_link_url' => '/fasilitas',
+
+	'home_feature_column_2_title' => 'Lingkungan Nyaman',
+	'home_feature_column_2_content' => 'Kami sangat memperhatikan kenyamanan lingkungan bagi para siswa dalam belajar',
+	'home_feature_column_2_link_text' => 'Daftar',
+	'home_feature_column_2_link_url' => '',
+
+	'home_feature_column_3_title' => 'Kurikulum Update',
+	'home_feature_column_3_content' => 'Kurikulum kami sangat menyesuaikan dengan kebutuhan dunia kerja militer saat ini',
+	'home_feature_column_3_link_text' => 'Lihat Kurikulum',
+	'home_feature_column_3_link_url' => '/kurikulum',
+
 ];
