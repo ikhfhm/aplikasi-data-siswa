d3103dea3ac320f593399ec6d3bc475413076099
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-23 08:13:19 +0700

    memodifikasi text pada register.blade.php

--- resources/views/sites/register.blade.php
+++ resources/views/sites/register.blade.php
@@ -7,9 +7,9 @@
 		<div class="row d-flex align-items-center justify-content-center">
 			<div class="about-content col-lg-12">
 				<h1 class="text-white">
-					About Us				
+					Pendaftaran				
 				</h1>	
-				<p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="about.html"> About Us</a></p>
+				<p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="about.html"> Pendaftaran</a></p>
 			</div>	
 		</div>
 	</div>
@@ -20,17 +20,17 @@
 		<div class="row justify-content-between align-items-center">
 			<div class="col-lg-6 col-md-6 search-course-left">
 				<h1>
-					Get reduced fee <br>
-					during this Summer!
+					Pendaftaran Online <br>
+					Bergabung bersama kami
 				</h1>
 				<p>
-					inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach.
+					Dengan kurikulum yang update dengan kebutuhan pasar, kami menjami lulusan akan mudah terserap di dunia kerja.
 				</p>
-				
+
 			</div>
 			<div class="col-lg-4 col-md-6 search-course-right section-gap">
 				<form class="form-wrap" action="#">
-					<h4 class="pb-20 text-center mb-30">Search for Available Course</h4>		
+					<h4 class="pb-20 text-center mb-30">Formulir Pendaftaran</h4>		
 					<input type="text" class="form-control" name="name" placeholder="Your Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Name'">
 					<input type="phone" class="form-control" name="phone" placeholder="Your Phone Number" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Phone Number'">
 					<input type="email" class="form-control" name="email" placeholder="Your Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your Email Address'">
