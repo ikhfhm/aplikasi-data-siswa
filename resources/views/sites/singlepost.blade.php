bf92fc7b8125ded1b6ae0a07714cde0545d90e6e
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-24 11:20:49 +0700

    mengganti isi konten dengan dolarpost->content pada singlepost.blade.php

--- resources/views/sites/singlepost.blade.php
+++ resources/views/sites/singlepost.blade.php
@@ -50,15 +50,7 @@
 								</div>
 								<div class="col-lg-9 col-md-9">
 									<h3 class="mt-20 mb-20">{{$post->title}}</h3>
-									<p class="excert">
-										MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction.
-									</p>
-									<p>
-										Boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed
-									</p>
-									<p>
-										Boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot camp when you can get the MCSE study materials yourself at a fraction of the camp price. However, who has the willpower to actually sit through a self-imposed MCSE training. who has the willpower to actually sit through a self-imposed
-									</p>
+									{{$post->content}}
 								</div>
 								<div class="col-lg-12">
 									<div class="quotes">
