64786c95a426ca4b0376ff5337914cc0cf892a66
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-24 17:01:52 +0700

    menambahkan protected dates pada model Post.php

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+	protected $dates = ['created_at'];
+
     public function user()
     {
     	return $this->belongsTo(User::class);
