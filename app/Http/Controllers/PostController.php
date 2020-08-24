920836aad71fa14c0efee611aa11c9bb0d1b5e0b
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-24 08:45:40 +0700

    menambahkan return view pada PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,5 +10,7 @@ class PostController extends Controller
     public function index()
     {
     	$posts = Post::all();
+    	$users = User::all();
+    	return view('posts.index',compact(['paste']));
     }
 }
