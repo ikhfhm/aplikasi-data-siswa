38a80794aae965238e421ee53e13b3e474c45855
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-26 00:30:21 +0700

    menambahkan cource code dari sluggabble

--- app/Post.php
+++ app/Post.php
@@ -11,6 +11,20 @@ class Post extends Model
 	protected $fillable = ['title', 'content', 'thumbnail', 'slug', 'user_id'];
 	protected $dates = ['created_at'];
 
+	/**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
     public function user()
     {
     	return $this->belongsTo(User::class);
