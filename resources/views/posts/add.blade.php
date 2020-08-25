6cbc500d9b266f03ad842d982cac3deab9bdf52f
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-25 10:35:42 +0700

    menambahkan source code submit button dari laravel file manager pada add.blade.php

--- resources/views/posts/add.blade.php
+++ resources/views/posts/add.blade.php
@@ -30,7 +30,15 @@
 										</div>
 
 										<div class="col-md-4">
-											Ini untuk thumbnail
+											 <div class="input-group">
+											   <span class="input-group-btn">
+											     <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+											       <i class="fa fa-picture-o"></i> Choose
+											     </a>
+											   </span>
+											   <input id="thumbnail" class="form-control" type="text" name="filepath">
+											 </div>
+											 <img id="holder" style="margin-top:15px;max-height:100px;">
 										</div>
 									</div>
 								</div>
