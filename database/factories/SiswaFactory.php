c165ee986e80bb6d77560c13ec5d5be8ff8f0095
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-26 23:40:04 +0700

    menambahkan nama field pada faker di SiswaFactory.php

--- database/factories/SiswaFactory.php
+++ database/factories/SiswaFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        'nama_depan' => $faker->name,
+        'nama_belakang' => '',
+        'jenis_kelamin' => $faker->randomElement(['L','P']),
+        'agama' => $faker->randomElement(['Islam','Kriten', 'Katolik', 'Hindu', 'Budha']),
+        'alamat' => $faker->adress,
     ];
 });
