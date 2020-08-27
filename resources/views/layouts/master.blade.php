a646531ab2bd83e23bbdfcf6bc4d42d1c91f7476
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-27 13:37:55 +0700

    instalasi Data Tables

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,6 +17,7 @@
   <!-- GOOGLE FONTS -->
   <!--TOASTR -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
   <!-- ICONS -->
   <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
@@ -57,6 +58,8 @@
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
   <script src="{{asset('frontend/js/ckeditor.js')}}"></script>
+  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
+
   <script>
     @if(Session::has('sukses'))
       toastr.success("{{Session::get('sukses')}}!", "Selamat")
