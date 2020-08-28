dd2c56c850f1a3b345d00fb6713f18fbb2d3c720
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 06:31:00 +0700

    add relation User with Komentar

--- app/User.php
+++ app/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Forum::class);
     }
+
+    public function komentar()
+    {
+        return $this->hasMany(Komentar::class);
+    }
 }
