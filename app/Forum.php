13272c8abe32d2d9d61d6455278398970a7d98c9
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 06:24:53 +0700

    add relation Forum with User

--- app/Forum.php
+++ app/Forum.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Forum extends Model
 {
     protected $table = 'forum';
+
+    public funtion user(){
+        return $this->belongsTo(User::class);
+    }
 }
