cf1289ce10ea7077b2015d95114e7c6d52adaeda
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 06:47:57 +0700

    add function index pada ForumController + use Forum;

--- app/Http/Controllers/ForumController.php
+++ app/Http/Controllers/ForumController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Forum;
 
 class ForumController extends Controller
 {
-    //
+    public function index(){
+        $forum = Forum::paginate(10);
+        return view('forum.index',compact(['forum']));
+    }
 }
