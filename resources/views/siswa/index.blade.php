ad118e8889e79a2d913d1f1e2ddfcd47f8ef7b97
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-28 13:34:40 +0700

    add button for import

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -63,6 +63,8 @@
 		      <div class="modal-footer">
 		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
 		        <button type="button" class="btn btn-primary">Save changes</button>
+		        <input type="submit" class="btn btn-sm btn-primary" value="import">
+		    	</form>
 		      </div>
 		    </div>
 		  </div>
