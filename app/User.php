0d6c5625ea0a42015bbddd66c5f9773c4668c33e
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 06:27:46 +0700

    add relation model User with Forum

--- app/User.php
+++ app/User.php
@@ -37,4 +37,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Siswa::class);
     }
+
+    public function forum()
+    {
+        return $this->hasMany(Forum::class);
+    }
 }
