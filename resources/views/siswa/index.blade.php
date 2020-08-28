44bea866b7e6636cbfc3345c5ec7e4be92365135
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-28 13:35:39 +0700

    delete button close dan save changes

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -61,8 +61,6 @@
 		        {!!Form::file('data_siswa')!!}
 		      </div>
 		      <div class="modal-footer">
-		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-		        <button type="button" class="btn btn-primary">Save changes</button>
 		        <input type="submit" class="btn btn-sm btn-primary" value="import">
 		    	</form>
 		      </div>
