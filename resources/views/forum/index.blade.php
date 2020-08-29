3893b1319e09f0e5f8320cbd46b11d237bc163d7
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 08:07:06 +0700

    edit btn class

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -10,7 +10,7 @@
 								<div class="panel-heading">
 									<h3 class="panel-title">Forum</h3>
 									<div class="right">
-                                        <a type="button" class="btn" data-toggle="modal" data-target="#exampleModal">Tambah Forum</i></a>
+                                        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Tambah Forum</i></a>
 									</div>
 								</div>
 								<div class="panel-body">
