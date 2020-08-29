574f61cfcee89f866de455354015375a219cb148
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 07:14:07 +0700

    change name panel title 'Forum'

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -8,7 +8,7 @@
 					<div class="col-md-12">
 						<div class="panel">
 								<div class="panel-heading">
-									<h3 class="panel-title">Posts</h3>
+									<h3 class="panel-title">Forum</h3>
 									<div class="right">
 										<a href="{{route('posts.add')}}" class="btn btn-sm btn-primary">Add New Post</a>
 									</div>
