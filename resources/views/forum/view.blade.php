a7804252940832e43c3b97d999f7856e47280a2d
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 09:13:04 +0700

    delete pannel heading view forum

--- resources/views/forum/view.blade.php
+++ resources/views/forum/view.blade.php
@@ -7,12 +7,7 @@
 				<div class="row">
 					<div class="col-md-12">
 						<div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Forum</h3>
-									<div class="right">
-                                        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Tambah Forum</i></a>
-									</div>
-								</div>
+								
 								<div class="panel-body">
                                 
 								</div>
