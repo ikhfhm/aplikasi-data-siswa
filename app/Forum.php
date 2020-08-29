9625bd0671c10e6b84bbc04e003169086bffff2d
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 08:22:23 +0700

    add sluggable pada Forum

--- app/Forum.php
+++ app/Forum.php
@@ -7,7 +7,22 @@ use Cviebrock\EloquentSluggable\Sluggable;
 
 class Forum extends Model
 {
+    use Sluggable;
     protected $table = 'forum';
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'judul'
+            ]
+        ];
+    }
 
     public function user(){
         return $this->belongsTo(User::class);
