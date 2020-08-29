95352abe5e6457ae91cb4a216f046472d4cbb529
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 09:12:35 +0700

    copy source code for view.blade.php

---
+++ resources/views/forum/view.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="main">
+		<div class="main-content">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="panel">
+								<div class="panel-heading">
+									<h3 class="panel-title">Forum</h3>
+									<div class="right">
+                                        <a type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Tambah Forum</i></a>
+									</div>
+								</div>
+								<div class="panel-body">
+                                
+								</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
