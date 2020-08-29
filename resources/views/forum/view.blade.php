e657029f3097f6c284202385777b99fbbb3455ec
ikhfhm <ikhwanfahmihidayat@gmail.com>
2020-08-29 09:27:41 +0700

    add panel headline

--- resources/views/forum/view.blade.php
+++ resources/views/forum/view.blade.php
@@ -6,11 +6,16 @@
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-md-12">
-						<div class="panel">
-								<div class="panel-body">
-                                
-								</div>
-						</div>
+                    <div class="panel panel-headline">
+				<div class="panel-heading">
+					<h3 class="panel-title">Panel Headline</h3>
+					<p class="panel-subtitle">Panel to display most important information</p>
+				</div>
+				<div class="panel-body">
+					<h4>Panel Content</h4>
+					<p>Objectively network visionary methodologies via best-of-breed users. Phosfluorescently initiate go forward leadership skills before an expanded array of infomediaries. Monotonectally incubate web-enabled communities rather than process-centric.</p>
+				</div>
+			</div>
 					</div>
 				</div>
 			</div>
